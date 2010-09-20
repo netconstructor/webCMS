dd11a0e8532052465acf7797eb723ad62606f0a7
Jeljer te Wies <jeljer@me.com>
2010-09-20 23:46:51 +0200

    fixed bug with attachement in ckeditor

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -21,9 +21,9 @@ CKEDITOR.editorConfig = function( config )
   //config.startupFocus = true;
   
   // works only with en, ru, uk languages
-//	var dialogName = config.data.name;
-//	var dialogDefinition = config.data.definition;
-	config.extraPlugins = "embed,attachment";
+	//	var dialogName = config.data.name;
+	//	var dialogDefinition = config.data.definition;
+	//config.extraPlugins = "attachment";
   config.resize_enabled = false;
 	config.toolbarCanCollapse = false;
   config.toolbar = 'Easy';
@@ -35,6 +35,6 @@ CKEDITOR.editorConfig = function( config )
         ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
         ['Link','Unlink'],
 				['Table','HorizontalRule','SpecialChar'],
-				['Image', 'Attachment']
+				['Image']
     ];
 };
