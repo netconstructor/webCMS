7fcc571e2639182f7becb24ca45b474a5f06bc36
Jeljer te Wies <jeljer@me.com>
2010-10-19 15:21:27 +0200

    images now can contain names

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,5 @@ class Gallery < ActiveRecord::Base
   belongs_to  :client
   has_many    :items, :as => :link
   has_many    :images, :dependent => :delete_all
+  accepts_nested_attributes_for :images, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100921181048) do
+ActiveRecord::Schema.define(:version => 20101019124009) do
 
   create_table "articles", :force => true do |t|
     t.text     "body"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(:version => 20100921181048) do
     t.datetime "data_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "items", :force => true do |t|
