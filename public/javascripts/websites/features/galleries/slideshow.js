403e8d7334a187e9a605bdc8592743e8417640e8
Jeljer te Wies <jeljer@me.com>
2010-11-24 22:36:57 +0100

    changed slideshow capabilities

--- public/javascripts/websites/features/galleries/slideshow.js
+++ public/javascripts/websites/features/galleries/slideshow.js
@@ -1,23 +1,29 @@
 function slideShow(speed) {
 	//append a LI item to the UL list for displaying caption
-	$('ul.slideshow').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><h3></h3><p></p></div></li>');
+  $('ul.slideshow').append('<li id="slideshow-caption" class="caption"><div class="slideshow-caption-container"><h3></h3><p></p></div></li>');
 
 	//Set the opacity of all images to 0
-	$('ul.slideshow li').css({opacity: 0.0});
+  $('ul.slideshow li').css({opacity: 0.0});
 	
 	//Get the first image and display it (set it to full opacity)
-	$('ul.slideshow li:first').css({opacity: 1.0});
+  $('ul.slideshow li:first').css({opacity: 1.0});
+  $('ul.slideshow li:first').addClass('show');
 	
 	//Get the caption of the first image from REL attribute and display it
-	$('#slideshow-caption h3').html($('ul.slideshow a:first').find('img').attr('title'));
-	$('#slideshow-caption p').html($('ul.slideshow a:first').find('img').attr('alt'));
+	$('#slideshow-caption h3').html($('ul.slideshow :first').find('img').attr('title'));
+	$('#slideshow-caption p').html($('ul.slideshow :first').find('img').attr('alt'));
 		
 	//Display the caption
-	$('#slideshow-caption').css({opacity: 0.7, bottom:0});
-	
+  var string = '';
+  string += $('ul.slideshow :first').find('img').attr('title');
+  string += $('ul.slideshow :first').find('img').attr('alt');
+  if(string == ''){
+    $('#slideshow-caption').css({opacity: 0.7, bottom:-70});
+  } else {
+    $('#slideshow-caption').css({opacity: 0.7, bottom:0});
+  }	
 	//Call the gallery function to run the slideshow	
-	var timer = setInterval('gallery()',speed);
-	
+  var timer = setInterval('gallery()',speed);
 	//pause the slideshow on mouse over
 	$('ul.slideshow').hover(
 		function () {
@@ -44,15 +50,17 @@ function gallery() {
 
 	//Set the fade in effect for the next image, show class has higher z-index
 	next.css({opacity: 0.0}).addClass('show').animate({opacity: 1.0}, 1000);
-	
-	//Hide the caption first, and then set and display the caption
-	$('#slideshow-caption').animate({bottom:-70}, 300, function () {
+  if(title == '' && desc == ''){
+    $('#slideshow-caption').animate({bottom:-70}, 300); 
+  } else {
+  	//Hide the caption first, and then set and display the caption
+	  $('#slideshow-caption').animate({bottom:-70}, 300, function () {
 			//Display the content
 			$('#slideshow-caption h3').html(title);
 			$('#slideshow-caption p').html(desc);				
 			$('#slideshow-caption').animate({bottom:0}, 500);	
-	});		
-
+	  });		
+  }
 	//Hide the current image
 	current.animate({opacity: 0.0}, 1000).removeClass('show');
 
